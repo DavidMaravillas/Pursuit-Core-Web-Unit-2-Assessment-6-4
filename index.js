d60302e48f2896a6cd841b653794526744486b98
david maravillas <davidmaravillas@pursuit.org>
2019-12-18 21:58:06 -0500

    progresses

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let filmList = document.querySelector("#filmList")
 let filmQuery = document.querySelector("#filmQuery")
+let filmReview = document.querySelector("#film_review")
+let display = document.querySelector("#display")
 let selectedFilm = ""
-let reviews = {}
+let currentMovieTitle = ""
 
 const appendFilmList = (arr) =>{
     arr.forEach(el=>{
@@ -23,15 +25,76 @@ const fetchMovieList = async ()=>{
     }
 }
 
-const fetchMovieInfo = (url)=>{
+const fetchMovieInfo = async (url)=>{
+    try {
+        display.innerHTML = ""
+        let film = await axios.get(url)
+        movieInfo = film.data
+        let h3 = document.createElement("h3")
+        let releaseYear = document.createElement("p")
+        let desc = document.createElement("p")
+        h3.innerText = movieInfo["title"]
+        releaseYear.innerText = movieInfo["release_date"]
+        desc.innerText = movieInfo["description"]
+        display.appendChild(h3)
+        display.appendChild(releaseYear)
+        display.appendChild(desc)
+        currentMovieTitle = movieInfo["title"]
 
+    } catch (err){
+        console.log(err)
+    }
+
+}
+
+const buildReview = () =>{
+    let userReview = document.querySelector("userReview")
+    let form = document.createElement("form")
+        let input = document.createElement("input")
+        let submit = document.createElement("input")
+
+        form.action = "submit"
+        form.id = "userReview"
+        input.type = "text"
+        input.id = "review"
+        submit.innerText = "Submit Review"
+        submit.type = "submit"
+        form.appendChild(input)
+        form.appendChild(submit)
+    let reviewUl = document.createElement("ul")
+
+    reviewUl.id = "reviewUl"
+    form.appendChild(reviewUl)
+    filmReview.appendChild(form)
 }
 
 fetchMovieList()
 
-filmList.addEventListener("change",() =>{
-    selectedFilm = filmList.value
-    
+
+filmList.addEventListener("change",(event) =>{
+    selectedFilm = event.currentTarget.value
 })
 
-fetchMovieInfo()
+buildReview()
+
+filmQuery.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    fetchMovieInfo(selectedFilm)
+    let userReview = document.querySelector("#userReview")
+    userReview.addEventListener("submit", (event)=>{
+        event.preventDefault()
+        let review = document.querySelector("#review")
+        let reviewUl = document.querySelector("#reviewUl")
+        if(!review.value){
+            alert("submit a review")
+        } else {
+            let li = document.createElement("li")
+            currentMovieTitle
+            li.innerText = `${currentMovieTitle} ${review.value}`
+            reviewUl.appendChild(li)
+        }
+        
+    })
+})
+
+
